e56643fad0658704a11ac6438fc2e5976f346603
晚梦 <127290936+LateDreamXD@users.noreply.github.com>
2025-06-01 16:26:14 +0800

    init(uno api): init with uno.2-pre3

--- src/main.js
+++ src/main.js
@@ -1,13 +1,26 @@
-// 运行在 Electron 主进程 下的插件入口
+const { ipcMain } = require('electron');
+const fs = require('fs');
+const path = require('path');
 
-
-// 创建窗口时触发
-exports.onBrowserWindowCreated = (window) => {
-    // window 为 Electron 的 BrowserWindow 实例
+const LLQQNTuno = {
+	api: {
+		plugin: {
+			rmdata(slug) {
+				const dest_path = path.join(LiteLoader.path.data, slug);
+				if(!fs.existsSync(dest_path)) return false;
+				fs.rmSync(dest_path, {recursive: true, force: true});
+			}
+		}
+	}
 }
 
+Object.defineProperty(globalThis, 'LLQQNTuno', {
+	configurable: false,
+});
 
-// 用户登录时触发
-exports.onLogin = (uid) => {
-    // uid 为 账号 的 字符串 标识
-}
+ipcMain.handle('LiteLoader.unofficial.api', (event, name, method, args) => {
+	try {
+		if (name == method) return LiteLoader.api[method](...args);
+		else return LiteLoader.api[name][method](...args);
+	} catch {return null;}
+});
