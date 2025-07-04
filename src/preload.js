f67fa6973d5ea98821b037cc7a7c56970877fdea
晚梦 <127290936+LateDreamXD@users.noreply.github.com>
2025-07-04 17:31:41 +0800

    sync(preload): sync funcs from main

--- src/preload.js
+++ src/preload.js
@@ -6,8 +6,14 @@ function invokeUNO(name, method, args) {
 
 contextBridge.exposeInMainWorld('LLQQNTuno', {
 	api: {
+		app: {
+			restart(...args) {return invokeUNO('app','restart', args);},
+			openURL(...args) {return invokeUNO('app', 'openURL', args);},
+		},
 		plugin: {
-			rmdata(...args) {return invokeUNO('plugin', 'rmdata', args);}
+			rmdata(...args) {return invokeUNO('plugin', 'rmdata', args);},
+			install(...args) {return invokeUNO('plugin', 'install', args);},
+			hotload(...args) {return invokeUNO('plugin', 'hotload', args);}
 		}
 	}
 });
